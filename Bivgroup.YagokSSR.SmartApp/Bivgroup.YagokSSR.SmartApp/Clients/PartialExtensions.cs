749fc02e99879bcac78b0c2ffc4bf9cdb5c06c96
av.soldatov <a.soldatov@bivgroup.ru>
2024-09-27 17:21:51 +0300

    Static files reroute hotfix

---
+++ Bivgroup.YagokSSR.SmartApp/Bivgroup.YagokSSR.SmartApp/Clients/PartialExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+using Bivgroup.YagokSSR.SmartApp.Helpers;
+
+namespace Bivgroup.YagokSSR.SmartApp.Scada
+{
+    public partial class ScadaClient
+    { 
+       partial void PrepareRequest(System.Net.Http.HttpClient client, System.Net.Http.HttpRequestMessage request, System.Text.StringBuilder urlBuilder)
+        {
+       // request.Headers.Add("Authorization", ApiHelper.auth_token);
+    }
+}
+}
+namespace Bivgroup.YagokSSR.SmartApp.Query
+{
+    public partial class QueryClient
+    {
+        partial void PrepareRequest(System.Net.Http.HttpClient client, System.Net.Http.HttpRequestMessage request, System.Text.StringBuilder urlBuilder)
+        {
+           // request.Headers.Add("Authorization", ApiHelper.auth_token);
+        }
+    }
+}
